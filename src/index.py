f3d2ebe2d932b40b9f46e790ab24ef08bf370284
Dominic Nygård <dominicnygard0@gmail.com>
2025-11-09 16:44:09 +0200

    Exercise 6/7

--- src/index.py
+++ src/index.py
@@ -1,19 +1,20 @@
 from varasto import Varasto
 
 
-def main():
-    mehua = Varasto(100.0)
-    olutta = Varasto(100.0, 20.2)
-
+def tulosta_luonti_tiedot(mehua, olutta):
     print("Luonnin jälkeen:")
     print(f"Mehuvarasto: {mehua}")
     print(f"Olutvarasto: {olutta}")
 
+
+def tulosta_olut_getterit(olutta):
     print("Olut getterit:")
     print(f"saldo = {olutta.saldo}")
     print(f"tilavuus = {olutta.tilavuus}")
     print(f"paljonko_mahtuu = {olutta.paljonko_mahtuu()}")
 
+
+def testaa_mehu_setterit(mehua):
     print("Mehu setterit:")
     print("Lisätään 50.7")
     mehua.lisaa_varastoon(50.7)
@@ -22,6 +23,8 @@ def main():
     mehua.ota_varastosta(3.14)
     print(f"Mehuvarasto: {mehua}")
 
+
+def testaa_virheelliset_varastot():
     print("Virhetilanteita:")
     print("Varasto(-100.0);")
     huono = Varasto(-100.0)
@@ -31,6 +34,8 @@ def main():
     huono = Varasto(100.0, -50.7)
     print(huono)
 
+
+def testaa_liika_lisays(olutta, mehua):
     print(f"Olutvarasto: {olutta}")
     print("olutta.lisaa_varastoon(1000.0)")
     olutta.lisaa_varastoon(1000.0)
@@ -41,17 +46,42 @@ def main():
     mehua.lisaa_varastoon(-666.0)
     print(f"Mehuvarasto: {mehua}")
 
+
+def testaa_liika_otto(olutta, mehua):
+    testaa_olut_otto(olutta)
+    testaa_mehu_otto(mehua)
+
+
+def testaa_olut_otto(olutta):
     print(f"Olutvarasto: {olutta}")
     print("olutta.ota_varastosta(1000.0)")
     saatiin = olutta.ota_varastosta(1000.0)
     print(f"saatiin {saatiin}")
     print(f"Olutvarasto: {olutta}")
 
+
+def testaa_mehu_otto(mehua):
     print(f"Mehuvarasto: {mehua}")
     print("mehua.otaVarastosta(-32.9)")
     saatiin = mehua.ota_varastosta(-32.9)
     print(f"saatiin {saatiin}")
     print(f"Mehuvarasto: {mehua}")
+
+
+def testaa_virhetilanteita_ja_ylityksia(olutta, mehua):
+    testaa_virheelliset_varastot()
+    testaa_liika_lisays(olutta, mehua)
+    testaa_liika_otto(olutta, mehua)
+
+
+def main():
+    mehua = Varasto(100.0)
+    olutta = Varasto(100.0, 20.2)
+
+    tulosta_luonti_tiedot(mehua, olutta)
+    tulosta_olut_getterit(olutta)
+    testaa_mehu_setterit(mehua)
+    testaa_virhetilanteita_ja_ylityksia(olutta, mehua)
 
 
 if __name__ == "__main__":
